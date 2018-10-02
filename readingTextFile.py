e58f0f5f789ccfe960266a144969c2a91f968b96
Sreekanth123 <sreekanthpendiyala@gmail.com>
2018-10-03 00:17:23 +0800

    Update readingTextFile.py
    Detailed steps of the program

--- readingTextFile.py
+++ readingTextFile.py
@@ -8,9 +8,19 @@ def splitComma(line: str):
     return "{}, {}".format(splits[1], splits[6])
 
 if __name__ == "__main__":
+    
+    #Creating the application with setting configuration
     conf = SparkConf().setAppName("readTextFileData").setMaster("local[*]")
     sc = SparkContext(conf = conf)
+    
+    #Reading the text file with spark context
     readTextFileData = sc.textFile("in/readTextFileData.text")
+    
+    #Filtering each line with value greater then 40
     readTextFileDataInUSA = readTextFileData.filter(lambda line: float(Utils.COMMA_DELIMITER.split(line)[6]) > 40)
+    
+    #In each line getting only specific columns from file
     readTextFileDataNameAndCityNames = readTextFileDataInUSA.map(splitComma)
+    
+    #Writing output of the program into file
     readTextFileDataNameAndCityNames.saveAsTextFile("out/readTextFileData_by_latitude.text")
