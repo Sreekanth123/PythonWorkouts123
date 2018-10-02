eee073431c9b4ebdaeb5596242c9a7fbf445fc00
Sreekanth123 <sreekanthpendiyala@gmail.com>
2018-10-03 00:10:08 +0800

    Update readingTextFile.py

--- readingTextFile.py
+++ readingTextFile.py
@@ -10,11 +10,7 @@ def splitComma(line: str):
 if __name__ == "__main__":
     conf = SparkConf().setAppName("readTextFileData").setMaster("local[*]")
     sc = SparkContext(conf = conf)
-    
     readTextFileData = sc.textFile("in/readTextFileData.text")
-
     readTextFileDataInUSA = readTextFileData.filter(lambda line: float(Utils.COMMA_DELIMITER.split(line)[6]) > 40)
-    
     readTextFileDataNameAndCityNames = readTextFileDataInUSA.map(splitComma)
-
     readTextFileDataNameAndCityNames.saveAsTextFile("out/readTextFileData_by_latitude.text")
